985bae0d467e33b088953070d0e9f4c2dad1201d
hoanggbao <kaitou.kurosaki@gmail.com>
2024-03-25 10:03:37 +0700

    init project with shadcn ui

--- lib/defination.ts
+++ lib/defination.ts
@@ -1,37 +1,47 @@
+export enum Role {
+  NONE,
+  USER,
+  WRITER,
+  MODERATOR,
+  ADMIN
+}
+
 export type UserType = {
   id: string;
   name: string;
   email?: string;
   image?: string;
   createdAt?: Date;
-  thread?: Thread[];
-  comment?: Comment[]; 
+  threads?: Thread[];
+  comments?: Comment[];
+  role: Role;
 };
 
-export type Category = {
+export type CategoryType = {
   id: string;
-  title: string;
   slug: string;
-  color: string | null;
-  thread: Thread[];
-  comment: Comment;
+  title: string;
+  desc?: string;
+  color: string;
   createdAt: Date;
+  threads?: Thread[];
 };
 
 export interface Thread {
   id: string;
   slug: string;
   title: string;
-  createdAt: string;
-  desc: string;
+  desc?: string;
   content: string;
   thumbnail?: string;
   views: Number;
+  createdAt: string;
   catSlug: string;
   userEmail: string;
-  cat: Category;
+  cat: CategoryType;
   user: UserType;
-  comments?: Comment[];
+  comments?: CommentType[];
+  vote?: VoteType[]
 }
 
 export interface SingleThread extends Thread {
@@ -39,11 +49,32 @@ export interface SingleThread extends Thread {
   _count: { comments: number };
 }
 
-export type Comment = {
+export type CommentType = {
   id: string;
   content: string;
   threadSlug: string;
   userEmail: string;
   createdAt: Date;
   user: UserType;
+  vote?: VoteType[]
 };
+
+export type VoteType = {
+  id: string;
+  type: 'upvote' | 'downvote'
+  userEmail: string;
+  user: UserType;
+}
+
+export type NotificationType = {
+  id: string;
+  title: string;
+  content: string;
+  createdAt: Date;
+  startFrom: Date;
+  endTo?: Date;
+  isExpired: boolean;
+  isStarted: boolean;
+  userEmail: string;
+  user: UserType;
+}
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,12 +1,6 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
-export function formatDate(date: string) {
-  const value = date.split("T");
-
-  return `${value[0]} ${value[1].slice(0, 5)}`;
+  return twMerge(clsx(inputs))
 }
